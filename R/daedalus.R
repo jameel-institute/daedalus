eedc9218852477248c8fdcbdd1f77b9f7ab8f113
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-11 11:49:05 +0100

    Update daedalus() and daedalus_ode() docs

--- R/daedalus.R
+++ R/daedalus.R
@@ -1,11 +1,12 @@
-#' DAEDALUS model for health, social and economic costs of a pandemic
+#' @title DAEDALUS model for health, social and economic costs of a pandemic
 #'
+#' @description Run the DAEDALUS model from R. This is a work in progress.
 #' @param initial_state The initial state of the population as a numeric matrix
 #' where rows represent age groups and columns represent epidemiological
 #' compartments.
 #' @param time_end An integer-like value (e.g. 100) for the number of timesteps
 #' at which to return data. This is treated as the number of days with data
-#' returned for each day.
+#' returned for each day. Defaults to 100 days.
 #' @param parameters A list of model parameters. See [default_parameters()] for
 #' model defaults.
 #'
