51c9d6185cb8680165d4bfe3b3c924c35cd01f83
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-19 16:16:47 +0100

    Update `daedalus()` to accept 3D array as init state

--- R/daedalus.R
+++ R/daedalus.R
@@ -1,9 +1,9 @@
 #' @title DAEDALUS model for health, social and economic costs of a pandemic
 #'
 #' @description Run the DAEDALUS model from R. This is a work in progress.
-#' @param initial_state The initial state of the population as a numeric matrix
-#' where rows represent age groups and columns represent epidemiological
-#' compartments.
+#' @param initial_state The initial state of the population as a numeric array
+#' where rows represent age groups, columns represent epidemiological
+#' compartments, and layers represent economic sectors.
 #' @param time_end An integer-like value for the number of timesteps
 #' at which to return data. This is treated as the number of days with data
 #' returned for each day. Defaults to 300 days.
@@ -15,20 +15,43 @@
 daedalus <- function(initial_state, time_end = 300L,
                      parameters = default_parameters()) {
   # NOTE: see constants.R for package constants
-  is_good_matrix <- checkmate::test_matrix(
+  is_array_numeric <- checkmate::test_array(
     initial_state, "numeric",
-    nrows = N_AGE_GROUPS, ncols = N_EPI_COMPARTMENTS,
     any.missing = FALSE
   )
-  if (!is_good_matrix) {
+  if (!is_array_numeric) {
+    cli::cli_abort(
+      "Expected `initial_state` to be a numeric {.cls {'array'}} with no
+      missing elements but it is {.obj_type_friendly {initial_state}} or has
+      missing elements."
+    )
+  }
+
+  is_good_array <- checkmate::test_array(
+    initial_state,
+    d = 3L
+  )
+  if (!is_good_array) {
+    cli::cli_abort(
+      "Expected `initial_state` to be a numeric {.cls {'array'}} with three
+      dimensions, but it has {length(dim(initial_state))} dimensions."
+    )
+  }
+
+  has_correct_dims <- identical(
+    dim(initial_state),
+    c(N_AGE_GROUPS, N_EPI_COMPARTMENTS, N_ECON_SECTORS)
+  )
+  if (!has_correct_dims) {
     cli::cli_abort(
       c(
-        "Expected `initial_state` to be a numeric matrix with {N_AGE_GROUPS}
-        rows and {N_EPI_COMPARTMENTS} columns with no missing values, but it has
-        {nrow(initial_state)} rows and {ncol(initial_state)} columns.",
+        "Expected `initial_state` to have dimensions
+        {c(N_AGE_GROUPS, N_EPI_COMPARTMENTS, N_ECON_SECTORS)},
+        but it has dimensions {dim(initial_state)}.",
         i = "The number of rows in `initial_state` represents the number of
-        age groups, while the number of columsn represents the number of
-        epidemiological compartments."
+        age groups, the number of columns represents the number of
+        epidemiological compartments, and the lenght of the third dimension
+        represents the number of economic sectors."
       )
     )
   }
