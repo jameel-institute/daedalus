9a8fc6be3eedb9952069de6803e4b5ef4a5002bc
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-30 16:36:29 +0100

    Update scripts in data-raw, upate Rda files

---
+++ data-raw/economic_contacts.R
@@ -0,0 +1,24 @@
+## code to prepare `economic_contacts` dataset goes here
+
+# notation
+# `cw`: contacts between workers within sectors
+# `cm_cw`: contacts between consumers and workers
+# `cm_ww`: contacts worker-to-worker between economic sectors
+
+#### process social contacts within economic sectors ####
+contacts_workplace <- fread("data-raw/sectorcontacts.csv")
+contacts_workplace <- contacts_workplace[["n_cnt"]]
+
+#### worker-to-worker contacts between sectors ####
+# prepare dummy values with low to no effect
+contacts_between_sectors <- matrix(
+  1.0e-6, N_ECON_SECTORS, N_ECON_SECTORS
+)
+diag(contacts_between_sectors) <- 0.0
+
+economic_contacts <- list(
+  contacts_workplace = contacts_workplace,
+  contacts_between_sectors = contacts_between_sectors
+)
+
+usethis::use_data(economic_contacts, overwrite = TRUE)
