2a296c250137406b6449d7e5cd54c3fdbb1db379
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-30 16:31:49 +0100

    Add country names and economic data

--- R/country_data.R
+++ R/country_data.R
@@ -18,10 +18,18 @@
 #'   \item{demography}{A four value named vector for the number of individuals
 #' in each demographic group.}
 #'   \item{contact_matrix}{A 16-element square matrix giving the mean number of
-#' social contacts between individuals of the four age groups}.
+#' social contacts between individuals of the four age groups.}
 #'   \item{workers}{A 45-element vector giving the number of individuals in each
 #' economic sector included in the model.}
 #' }
 #' @source \doi{10.1126/science.abc0035}; see processing details in
 #' `data-raw/country_data.R
 "country_data"
+
+#' @title Country names for DAEDALUS
+#'
+#' @description Names for countries and territories included with the package.
+#'
+#' @format ## `country_names`
+#' A character vector of recognised country or territory names.
+"country_names"
