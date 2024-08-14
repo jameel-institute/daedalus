bf3f2a4c1a0d8c5033c9613f0ad2209c39a0fcb9
Pratik Gupte <pratikgupte16@gmail.com>
2024-08-14 13:53:06 +0100

    Streamline checks, update function signature

--- R/daedalus.R
+++ R/daedalus.R
@@ -1,7 +1,14 @@
 #' @title DAEDALUS model for health, social and economic costs of a pandemic
 #'
 #' @description Run the DAEDALUS model from R. This is a work in progress.
+#'
 #' @param country A country or territory name from among `country_names`.
+#' Country-specific data such as the community and workplace contacts, the
+#' demography, and the distribution of the workforce into economic sectors is
+#' automatically accessed from package data for the relevant country name.
+#' Some contact data values can be over-ridden by the user by passing them as a
+#' named list to `country_params_manual`.
+#'
 #' @param epidemic A string for the infection parameter set to use.
 #' Infection parameter sets may be known by the name of the outbreak or the
 #' causative pathogen;
@@ -14,9 +21,20 @@
 #' associated with the epidemic; these are stored as packaged data in
 #' `infection_data`.
 #' Default infection parameters for epidemics can be over-ridden by passing them
-#' as named arguments via `...`.
-#' @param ... Additional arguments passed to `make_parameters()` or
-#' `make_initial_state()`. See **Details** for permitted arguments.
+#' as a named list to `infection_params_manual`.
+#'
+#' @param country_params_manual An optional **named** list of country-specific
+#' contact data. See **Details** for allowed values.
+#'
+#' @param infect_params_manual An optional **named** list of infection
+#' parameters that can be passed to over-ride the default values for the chosen
+#' `epidemic`. See **Details** for allowed values.
+#'
+#' @param initial_state_manual An optional **named** list with the names
+#' `p_infectious` and `p_asymptomatic` for the proportion of infectious and
+#' symptomatic individuals in each age group and economic sector.
+#' Defaults to `1e-6` and `0.0` respectively.
+#'
 #' @param time_end An integer-like value for the number of timesteps
 #' at which to return data. This is treated as the number of days with data
 #' returned for each day. Defaults to 300 days.
@@ -32,15 +50,16 @@
 #' Covid-19 Omicron (SARS-CoV-2 omicron), Covid-19 Delta (SARS-CoV-2 delta).
 #'
 #' DAEDALUS allows users to substitute default model parameters with custom
-#' values by passing them as `...` (see examples). These arguments are passed on
-#' to the internal functions `make_parameters()` and `make_initial_state()` as
+#' values by passing them in the appropriate argument (see examples).
+#' These arguments are passed on to the internal functions
+#' `make_country_parameters()` and `make_infection_parameters()` as
 #' appropriate. Any arguments that are passed and are not suitable for those
-#' functions will throw a warning.
+#' functions will throw an error.
 #'
 #' ## Initial state
 #'
-#' Users can pass the following initial state parameters, which are handled
-#' internally by `make_initial_state()`:
+#' Users can pass the following initial state parameters to
+#' `initial_state_manual`:
 #'
 #' - `p_infectious`: A single numeric value in the range \eqn{[0.0, 1.0]} giving
 #' the proportion of individuals in each age group and economic sector that are
@@ -51,10 +70,35 @@
 #' the proportion of initially infectious individuals who are considered to be
 #' asymptomatic. Defaults to 0.0.
 #'
-#' ## Model parameters
+#' ## Country parameters
 #'
-#' Users can pass the following infection and country parameters, which are
-#' handled internally by `make_parameters()`:
+#' `country_params_manual` may be a named list of one or more of the following:
+#'
+#' - `contact_matrix`: A square numeric matrix with `N_AGE_GROUPS` (4) rows and
+#' columns, giving the per-capita number of contacts between individuals of each
+#' age group. By default, this is taken from `daedalus::country_data` based on
+#' the user-specified `country`.
+#'
+#' - `contacts_workplace`: A numeric vector of length `N_ECON_SECTORS` (45),
+#' giving the per-capita number of worker-to-worker interactions within economic
+#' sectors. Defaults to a standard value provided by the package as
+#' `daedalus::economic_contacts`.
+#'
+#' - `contacts_consumer_worker`: A numeric matrix with `N_ECON_SECTORS` rows
+#' (45) and `N_AGE_GROUPS` columns (4) giving the per-capita contacts between
+#' individuals in each economic sector and consumers. Defaults to the
+#' sector-wise workplace contacts distributed in proportion to country
+#' demography.
+#'
+#' - `contacts_between_sectors`: A square numeric matrix with `N_ECON_SECTORS`
+#' (45) rows and columns and with a zero diagonal, giving the number of
+#' worker-to-worker contacts across economic sectors. Defaults to the package
+#' standard of a null matrix as between-sector-contacts are not currently
+#' modelled.
+#'
+#' ## Infection parameters
+#'
+#' `infect_params_manual` may be a named list of one or more of the following:
 #'
 #' - `r0`: A single numeric value for the basic reproduction value of the
 #' infection \eqn{R_0}.
@@ -90,32 +134,6 @@
 #' immunity wanes, returning individuals in the 'recovered' compartment to the
 #' 'susceptible' compartment.
 #'
-#' - `demography`: A numeric vector of length `N_AGE_GROUPS` (4), giving the
-#' number of individuals in each age group of interest. By default, this is
-#' taken from `daedalus::country_data` based on the `country` selected.
-#'
-#' - `contact_matrix`: A square numeric matrix with `N_AGE_GROUPS` (4) rows and
-#' columns, giving the per-capita number of contacts between individuals of each
-#' age group. By default, this is taken from `daedalus::country_data` based on
-#' the user-specified `country`.
-#'
-#' - `contacts_workplace`: A numeric vector of length `N_ECON_SECTORS` (45),
-#' giving the per-capita number of worker-to-worker interactions within economic
-#' sectors. Defaults to a standard value provided by the package as
-#' `daedalus::economic_contacts`.
-#'
-#' - `contacts_consumer_worker`: A numeric matrix with `N_ECON_SECTORS` rows
-#' (45) and `N_AGE_GROUPS` columns (4) giving the per-capita contacts between
-#' individuals in each economic sector and consumers. Defaults to the
-#' sector-wise workplace contacts distributed in proportion to country
-#' demography.
-#'
-#' - `contacts_between_sectors`: A square numeric matrix with `N_ECON_SECTORS`
-#' (45) rows and columns and with a zero diagonal, giving the number of
-#' worker-to-worker contacts across economic sectors. Defaults to the package
-#' standard of a null matrix as between-sector-contacts are not currently
-#' modelled.
-#'
 #' @return A `<deSolve>` object.
 #'
 #' @examples
@@ -125,44 +143,33 @@
 #' # with some infection parameters over-ridden by the user
 #' output <- daedalus(
 #'   country = "United Kingdom", epidemic = "influenza_1918",
-#'   r0 = 3.0
+#'   infect_params_manual = list(r0 = 1.3)
+#' )
+#'
+#' # with some country parameters over-ridden by the user
+#' output <- daedalus(
+#'   country = "United Kingdom", epidemic = "influenza_1918",
+#'   country_params_manual = list(contact_matrix = matrix(1, 4, 4))
+#' )
+#'
+#' # with default initial conditions over-ridden by the user
+#' output <- daedalus(
+#'   country = "United Kingdom", epidemic = "influenza_1918",
+#'   initial_state_manual = list(p_infectious = 1e-3)
 #' )
 #' @export
 daedalus <- function(country,
-                     epidemic = "sars_cov_2_pre_alpha",
-                     ...,
+                     epidemic,
+                     country_params_manual = list(),
+                     infect_params_manual = list(),
+                     initial_state_manual = list(),
                      time_end = 300) {
   # input checking
   # NOTE: names are case sensitive
-  is_good_country <- checkmate::test_string(country) &&
-    checkmate::test_subset(
-      country, daedalus::country_names,
-      empty.ok = FALSE
-    )
-  if (!is_good_country) {
-    cli::cli_abort(
-      c(
-        "Expected `country` to be a single string giving a country name from
-        among `country_names`.",
-        i = "Country names are case sensitive and must be an exact match."
-      )
-    )
-  }
-
-  is_good_epidemic <- checkmate::test_string(epidemic) &&
-    checkmate::test_subset(
-      epidemic, names(daedalus::infection_data),
-      empty.ok = FALSE
-    )
-  if (!is_good_epidemic) {
-    cli::cli_abort(
-      c(
-        "Expected `epidemic` to be a string from among `epidemic_names`, but
-        it is not.",
-        i = "Epidemic names are case sensitive and must be an exact match."
-      )
-    )
-  }
+  country <- rlang::arg_match(
+    country, daedalus::country_names
+  )
+  epidemic <- rlang::arg_match(epidemic, daedalus::epidemic_names)
 
   is_good_time_end <- checkmate::test_count(time_end, positive = TRUE)
   if (!is_good_time_end) {
@@ -174,37 +181,10 @@ daedalus <- function(country,
     )
   }
 
-  # capture parameters, check for numeric input, and split for lower-level fns
-  parameters <- list(...)
-
-  is_numeric_list <- checkmate::test_list(
-    parameters,
-    types = "numeric", any.missing = TRUE, all.missing = TRUE
-  )
-  if (!is_numeric_list) {
-    cli::cli_abort(
-      c(
-        "Model options passed as `...` may only be numeric elements (numbers,
-        and numeric vectors or matrices).",
-        i = "See {.help daedalus::daedalus} Details for allowed parameter names
-        and default values."
-      )
-    )
-  }
-
-  init_state_params <- parameters[names(parameters) %in%
-    c("p_infectious", "p_asymptomatic")]
-
-  initial_state <- do.call(
-    make_initial_state,
-    c(list(country = country), init_state_params)
-  )
-
-  parameters <- do.call(
-    make_parameters,
-    c(list(country = country, epidemic = epidemic), parameters[
-      !names(parameters) %in% names(init_state_params)
-    ])
+  initial_state <- make_initial_state(country, initial_state_manual)
+  parameters <- c(
+    make_country_parameters(country, country_params_manual),
+    make_infection_parameters(epidemic, infect_params_manual)
   )
 
   data <- deSolve::lsoda(
