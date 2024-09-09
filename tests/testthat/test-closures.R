d027829bf6681d6a649cb521229f91898772008c
Pratik Gupte <pratikgupte16@gmail.com>
2024-09-09 15:50:09 +0100

    More sanity checks for interventions

--- tests/testthat/test-closures.R
+++ tests/testthat/test-closures.R
@@ -27,7 +27,75 @@ test_that("Closures: basic expectations: runs without errors", {
   })
 })
 
-# expect that applying closures reduce epidemic size
+# expectation that response strategy 'none' has little to no effect
+test_that("Closures: no response leads to similar epidemic sizes", {
+  # both implementation levels have identical epidemic sizes
+  levels <- c("light", "heavy")
+  output_list <- lapply(levels, function(x) {
+    daedalus(
+      "Canada", "sars_cov_1",
+      response_strategy = "none", implementation_level = x
+    )
+  })
+
+  epidemic_sizes <- vapply(
+    output_list, function(x) {
+      sum(x[x$compartment == "recovered" & x$time == max(x$time), ]$value)
+    }, numeric(1)
+  )
+
+  expect_identical(
+    epidemic_sizes[1], epidemic_sizes[2]
+  )
+
+  # response time has no effect when strategy is 'none'
+  response_times <- c(10, 100)
+  response_threshold <- 1e7 # artificially large
+  output_list <- lapply(response_times, function(x) {
+    daedalus(
+      "Canada", "sars_cov_1",
+      response_strategy = "none", response_time = x,
+      response_threshold = response_threshold
+    )
+  })
+
+  epidemic_sizes <- vapply(
+    output_list, function(x) {
+      sum(x[x$compartment == "recovered" & x$time == max(x$time), ]$value)
+    }, numeric(1)
+  )
+  # NOTE high tolerance as the two have just slightly different final sizes
+  expect_identical(
+    epidemic_sizes[1], epidemic_sizes[2],
+    tolerance = 1L
+  )
+
+  # response threshold has no effect when strategy is 'none'
+  response_thresholds <- c(10, 1000)
+  time_end <- 300
+  response_time <- time_end - 2L # artificially long response time
+  output_list <- lapply(response_thresholds, function(x) {
+    daedalus(
+      "Canada", "sars_cov_1",
+      response_strategy = "none", response_time = response_time,
+      response_threshold = x
+    )
+  })
+
+  epidemic_sizes <- vapply(
+    output_list, function(x) {
+      sum(x[x$compartment == "recovered" & x$time == max(x$time), ]$value)
+    }, numeric(1)
+  )
+
+  # NOTE high tolerance as the two have just slightly different final sizes
+  expect_identical(
+    epidemic_sizes[1], epidemic_sizes[2],
+    tolerance = 1L
+  )
+})
+
+# expect that applying closures reduces epidemic size
 test_that("Closures: basic statistical correctness: reduces epidemic size", {
   output_list <- lapply(response_names, function(x) {
     daedalus(
@@ -51,8 +119,8 @@ test_that("Closures: basic statistical correctness: reduces epidemic size", {
 
 # expect that earlier closures reduce epidemic size
 test_that("Closures: earlier closures reduce epidemic size", {
-  response_times <- c(200, 10)
-  response_threshold <- 1e7 # very high to prevent auto-activation
+  response_times <- c(200, 30)
+  response_threshold <- 1e9 # very high to prevent auto-activation
   output_list <- lapply(response_times, function(x) {
     daedalus(
       "Canada", "sars_cov_1",
