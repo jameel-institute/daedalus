0651697ca8954253962610a0f9d30ce5ab8fbe0f
Pratik Gupte <pratikgupte16@gmail.com>
2024-09-13 15:05:55 +0100

    Update WORDLIST and tests for country_data with GVA and reduced countries

--- tests/testthat/test-country_data.R
+++ tests/testthat/test-country_data.R
@@ -1,13 +1,9 @@
 # snapshot tests for country data to flag unexpected changes
-seq <- seq(1, length(daedalus::country_data), 20) # approx 10
+# check some countries
+test_countries <- c("China", "United Kingdom", "Canada")
 test_that("daedalus::country_data: snapshot tests", {
   expect_snapshot(
-    daedalus::country_data[seq]
-  )
-
-  expect_identical(
-    daedalus::country_data[seq],
-    daedalus::country_data[daedalus::country_names[seq]]
+    daedalus::country_data[test_countries]
   )
 
   expect_snapshot(
