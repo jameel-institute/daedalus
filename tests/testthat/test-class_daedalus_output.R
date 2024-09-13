c6cfa36ead49e56dd10b9956a19fc44c095de4f9
Pratik Gupte <pratikgupte16@gmail.com>
2024-09-13 10:34:11 +0100

    Update get_data docs and test for output class

--- tests/testthat/test-class_daedalus_output.R
+++ tests/testthat/test-class_daedalus_output.R
@@ -1,7 +1,7 @@
 # Tests for the <daedalus_output> class
 test_that("class <daedalus_output>: basic expectations", {
   expect_no_condition({
-    output <- daedalus("Canada", "influenza_1918") # nolint saves extra assignment
+    output <- daedalus("Canada", "influenza_1918") # nolint saves assignment
   })
   expect_s3_class(output, "daedalus_output")
   checkmate::expect_list(
@@ -13,13 +13,22 @@ test_that("class <daedalus_output>: basic expectations", {
   )
 
   checkmate::expect_data_frame(
-    get_data(output), ncols = 5L
+    get_data(output),
+    ncols = 5L
   )
   checkmate::expect_list(
     get_data(output, "response_data")
   )
+  checkmate::expect_list(
+    get_data(output, "country_parameters")
+  )
+  # NOTE: tests pass but get_data.daedalus_output() is flagged as not tested
   expect_error(
     get_data(output, c("model_data", "response_data")),
-    regexp = "must be a single string"
+    regexp = "must be a single string naming an element of `x`"
+  )
+  expect_error(
+    get_data(output, "dummy_data"),
+    regexp = "must be a single string naming an element of `x`"
   )
 })
