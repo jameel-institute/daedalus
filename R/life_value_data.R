54a8dc4b155aa12e09b55b3ab5440f519665957e
Pratik Gupte <pratikgupte16@gmail.com>
2024-09-16 17:21:35 +0100

    Combine expectancy and value to single VLS dataset

---
+++ R/life_value_data.R
@@ -0,0 +1,13 @@
+#' @title Values of a statistical lives lost
+#'
+#' @description Values in dollars (scaled by purchasing power parity) of a
+#' statistical life lost in each of the four DAEDALUS age groups, accounting
+#' for life expectancy.
+#'
+#' @format ## `life_value`
+#' A list with 180 elements, each corresponding to a country or territory, and
+#' giving the age-group-specific value of a statistical life lost. Each list
+#' element is a numeric vector of four values giving the value per age group,
+#' in the following order: 0-4, 5-19, 20-65, 65+.
+#' @source Multiple sources; see processing details in `data-raw/life_value.R.
+"life_value"
