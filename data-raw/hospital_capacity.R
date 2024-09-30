668296080eab36489c45f65410e257c6146a6b6b
Pratik Gupte <pratikgupte16@gmail.com>
2024-09-30 10:16:34 +0100

    Update hospital capacity script

--- data-raw/hospital_capacity.R
+++ data-raw/hospital_capacity.R
@@ -4,21 +4,32 @@
 # total capacity * total population * (1 - bed occupancy rate)
 # NOTE: capacity is in beds per thousand people
 
-library(readxl)
+library(data.table)
 library(usethis)
 
-data_beds <- read_excel("inst/extdata/hospital_capacity.xlsx")
-data_occupancy <- read_excel(
-  "inst/extdata/hospital_capacity.xlsx",
-  sheet = "BOR"
-)
+# read data
+data_beds <- fread("inst/extdata/hospital_capacity_raw.csv")
+data_occupancy <- fread("inst/extdata/hospital_BOR.csv")
+
+# subset columns
 data_beds <- data_beds[, c("Country Name", "Capacity")]
 data_occupancy <- data_occupancy[, c("COUNTRY", "BOR")]
 
 colnames(data_beds) <- c("country", "capacity")
 colnames(data_occupancy) <- c("country", "bor")
 
-data_hosp_capacity <- merge(data_beds, data_occupancy)
+# remove columns with NA for the country
+data_beds <- data_beds[!is.na(country), ]
+data_occupancy <- data_occupancy[!is.na(country), ]
+
+# remove countries where the name is ""
+data_beds <- data_beds[country != "", ]
+data_occupancy <- data_occupancy[country != "", ]
+
+# merge data and assign missing BOR as 85%
+data_hosp_capacity <- merge(data_beds, data_occupancy, all.x = TRUE)
+data_hosp_capacity$bor <- nafill(data_hosp_capacity$bor, fill = 85.0)
+
 data_hosp_capacity$spare_capacity <- data_hosp_capacity$capacity *
   (1 - data_hosp_capacity$bor / 100)
 
