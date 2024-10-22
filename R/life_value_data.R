332c76661a55ab96407f83f4becb965e9529eb78
Pratik Gupte <pratikgupte16@gmail.com>
2024-10-22 16:31:52 +0100

    Country class provides GNI data

--- R/life_value_data.R
+++ R/life_value_data.R
@@ -1,13 +1,23 @@
 #' @title Values of statistical lives lost
 #'
+#' @name life_value
+#' @rdname life_value
+#'
 #' @description Values in dollars (scaled by purchasing power parity) of a
 #' statistical life lost in each of the four DAEDALUS age groups, accounting
 #' for life expectancy.
 #'
 #' @format ## `life_value`
-#' A list with 180 elements, each corresponding to a country or territory, and
+#' A list with 67 elements, each corresponding to a country or territory, and
 #' giving the age-group-specific value of a statistical life lost. Each list
 #' element is a numeric vector of four values giving the value per age group,
 #' in the following order: 0-4, 5-19, 20-65, 65+.
 #' @source Multiple sources; see processing details in `data-raw/life_value.R.
 "life_value"
+
+#' @name life_value
+#' @format ## `country_gni`
+#' A list with 67 elements, each corresponding to a country or territory, and
+#' giving the GNI (gross national income) in international dollars.
+#' @source Multiple sources; see processing details in `data-raw/life_value.R.
+"country_gni"
