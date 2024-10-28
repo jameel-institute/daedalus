59bc16fcf5e51673d12bbf02d09ad1776fcf3404
Pratik Gupte <pratikgupte16@gmail.com>
2024-10-28 09:23:10 +0000

    Add basic test for`get_beta()`

---
+++ tests/testthat/_snaps/r0_to_beta.md
@@ -0,0 +1,7 @@
+# Calculating beta from R0
+
+    Code
+      get_beta(x, y)
+    Output
+      [1] 0.01165953
+
---
+++ tests/testthat/test-r0_to_beta.R
@@ -0,0 +1,13 @@
+test_that("Calculating beta from R0", {
+  
+  x = daedalus_infection("sars_cov_1")
+  y = daedalus_country("United Kingdom")
+  
+  expect_no_condition(
+    get_beta(x, y)
+  )
+  
+  expect_snapshot(
+    get_beta(x, y)
+  )
+})
