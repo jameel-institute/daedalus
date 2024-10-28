3f38e70fd557b253014db2013b11c57d544d4d47
Pratik Gupte <pratikgupte16@gmail.com>
2024-10-28 09:23:34 +0000

    Style files

--- tests/testthat/test-r0_to_beta.R
+++ tests/testthat/test-r0_to_beta.R
@@ -1,12 +1,11 @@
 test_that("Calculating beta from R0", {
-  
-  x = daedalus_infection("sars_cov_1")
-  y = daedalus_country("United Kingdom")
-  
+  x <- daedalus_infection("sars_cov_1")
+  y <- daedalus_country("United Kingdom")
+
   expect_no_condition(
     get_beta(x, y)
   )
-  
+
   expect_snapshot(
     get_beta(x, y)
   )
