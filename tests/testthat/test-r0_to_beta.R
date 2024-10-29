746a4eecd0e77a4b65d313182a567986ad1f3f94
Pratik Gupte <pratikgupte16@gmail.com>
2024-10-29 08:27:30 +0000

    Add tests for R_eff

--- tests/testthat/test-r0_to_beta.R
+++ tests/testthat/test-r0_to_beta.R
@@ -20,6 +20,7 @@ test_that("Calculating beta from R0", {
     prepare_parameters.daedalus_infection(x),
     prepare_parameters.daedalus_country(y)
   )
+  parameters$beta <- get_beta(x, y)
 
   state <- make_initial_state(y, list())
 
@@ -41,8 +42,7 @@ test_that("Calculating beta from R0", {
   # NOTE: difference is due to a small number of infected
   r0 <- get_data(x, "r0")
   expect_identical(
-    r0 / z,
-    get_beta(x, y),
+    z, r0,
     tolerance = 1e-3
   )
 })
