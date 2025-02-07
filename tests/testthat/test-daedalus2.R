3dea726ca3ccbd7549faf0646f5ff41ac6a0a0e0
Pratik Gupte <pratikgupte16@gmail.com>
2025-02-07 10:10:38 +0000

    Update vignettes and tests

--- tests/testthat/test-daedalus2.R
+++ tests/testthat/test-daedalus2.R
@@ -1,11 +1,88 @@
-test_that("daedalus2 works", {
-  expect_no_condition(
-    daedalus2(10)
-  )
-  expect_no_condition(
-    daedalus2(10, n_strata = 2)
+# Tests for daedalus2() mirror `test-daedalus.R`
+country_canada <- daedalus_country("Canada")
+
+test_that("daedalus2: basic expectations", {
+  # expect no conditions
+  expect_no_condition({
+    daedalus2(country_canada, "influenza_1918")
+  })
+  output <- daedalus2(country_canada, "influenza_1918")
+
+  # expect list is type double and non-negative
+  checkmate::expect_list(output, "numeric", len = N_MODEL_COMPARTMENTS)
+  expect_true(
+    all(
+      vapply(output, function(x) all(x >= 0.0), FUN.VALUE = logical(1))
+    )
   )
 
-  output <- daedalus2(10, n_strata = 2)
-  expect_list(output, "numeric")
+  # expect closed population with no change in total size
+  pop_sizes_time <- Reduce(
+    x = lapply(output[i_EPI_COMPARTMENTS], colSums), f = `+`
+  )
+  pop_size <- sum(get_data(country_canada, "demography"))
+  expect_length(
+    unique(pop_size), 1L
+  )
+  expect_true(
+    all(abs(pop_sizes_time - pop_size) <= 1e-6)
+  )
+})
+
+test_that("Can run with ISO2 country parameter", {
+  expect_no_condition({
+    daedalus2("CA", "influenza_1918")
+  })
+
+  output <- daedalus2("CA", "influenza_1918")
+
+  # expect list is type double and non-negative
+  checkmate::expect_list(output, "numeric", len = N_MODEL_COMPARTMENTS)
+  expect_true(
+    all(
+      vapply(output, function(x) all(x >= 0.0), FUN.VALUE = logical(1))
+    )
+  )
+})
+
+test_that("Can run with ISO3 country parameter", {
+  expect_no_condition({
+    daedalus2("CAN", "influenza_1918")
+  })
+
+  output <- daedalus2("CAN", "influenza_1918")
+
+  # expect list is type double and non-negative
+  checkmate::expect_list(output, "numeric", len = N_MODEL_COMPARTMENTS)
+  expect_true(
+    all(
+      vapply(output, function(x) all(x >= 0.0), FUN.VALUE = logical(1))
+    )
+  )
+})
+
+# test that daedalus runs for all epidemic infection parameter sets
+test_that("daedalus: Runs for all country x infection x response", {
+  country_infection_combos <- data.table::CJ(
+    country = daedalus::country_names,
+    infection = daedalus::epidemic_names
+  )
+
+  time_end <- 10
+
+  # expect no conditions
+  invisible(
+    Map(
+      country_infection_combos$country,
+      country_infection_combos$infection,
+      f = function(x, y) {
+        expect_no_condition(
+          daedalus2(
+            x, y,
+            time_end = time_end
+          )
+        )
+      }
+    )
+  )
 })
