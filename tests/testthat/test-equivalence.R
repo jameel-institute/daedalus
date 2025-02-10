6f64f0f259647bf4af99cad188f67e0019e51808
Pratik Gupte <pratikgupte16@gmail.com>
2025-02-10 15:37:19 +0000

    Add test for limited `daedalus()` equivalence

---
+++ tests/testthat/test-equivalence.R
@@ -0,0 +1,31 @@
+test_that("daedalus() and daedalus2() are equivalent", {
+  # Excluding economic infections
+  x <- daedalus_infection("sars_cov_1", rho = 0.0)
+  ct <- daedalus_country("GBR")
+  # set workers to 1 to discount effect
+  ct$workers <- rep(1, length(ct$workers))
+  # modify vaccination to fit needs
+  vax <- daedalus_vaccination(
+    "none",
+    nu = 0.0, vax_start_time = 5, vax_uptake_limit = 0
+  )
+
+  # get outputs
+  output_daedalus2 <- daedalus2(ct, x, time_end = 399) # one less timestep
+  output_daedalus <- daedalus(
+    ct, x, "none", vax,
+    response_time = 2,
+    time_end = 400,
+    initial_state_manual = list(p_infectious = 1e-7)
+  )
+
+  # compare final sizes
+  fs_daedalus <- get_epidemic_summary(output_daedalus, "infections")
+  fs_daedalus2 <- sum(output_daedalus2$new_inf)
+
+  # a small difference is okay
+  expect_identical(
+    fs_daedalus2, fs_daedalus$value,
+    tolerance = 1e-6
+  )
+})
