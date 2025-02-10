3775959a7bfb52bd550dd5abf3e17d428164513a
Pratik Gupte <pratikgupte16@gmail.com>
2025-02-10 13:41:52 +0000

    Change header guards

--- inst/include/daedalus.h
+++ inst/include/daedalus.h
@@ -1,11 +1,8 @@
 // Copyright 2025 Imperial College of Science, Technology and Medicine.
 // See repository licence in LICENSE.md.
 
-#ifndef daedalus_daedalus_H_
-#define daedalus_daedalus_H_
+#pragma once
 
 // paths relative to this file
 #include "daedalus_constants.h"
 #include "daedalus_helpers.h"
-
-#endif  // daedalus_daedalus_H_
