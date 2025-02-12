e764ea4a6bab915eca30dc3b8c07ea2ea11d2564
Pratik Gupte <pratikgupte16@gmail.com>
2025-02-12 14:36:15 +0000

    Add Tensor indices as constants

--- inst/include/daedalus_constants.h
+++ inst/include/daedalus_constants.h
@@ -17,6 +17,10 @@ constexpr int N_COMPARTMENTS = N_EPI_COMPARTMENTS + N_DATA_COMPARTMENTS;
 constexpr int iS = 0, iE = 1, iIs = 2, iIa = 3, iH = 4, iR = 5, iD = 6, idE = 7,
               idH = 8;
 
+// Tensor indices for groups (age + econ sector), compartments, and vax strata
+// Assumes distinct vaccination pathways
+constexpr size_t i_GRPS = 0, i_COMPS = 1, i_VAX_GRPS = 2;
+
 // for C++ < 20
 inline const std::vector<size_t> i_DATA_COMPARTMENTS = {7, 8};
 inline const std::vector<size_t> seq_DATA_COMPARTMENTS = {8, 9};
