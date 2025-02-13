fe152e03646d95864257578fc47e585abb2f9301
Pratik Gupte <pratikgupte16@gmail.com>
2025-02-13 15:16:29 +0000

    `zero_which()` handles vax strata

--- inst/include/daedalus_constants.h
+++ inst/include/daedalus_constants.h
@@ -13,6 +13,7 @@ namespace constants {
 constexpr int N_EPI_COMPARTMENTS = 7;
 constexpr int N_DATA_COMPARTMENTS = 2;
 constexpr int N_COMPARTMENTS = N_EPI_COMPARTMENTS + N_DATA_COMPARTMENTS;
+constexpr int N_VAX_STRATA = 3;
 
 constexpr int iS = 0, iE = 1, iIs = 2, iIa = 3, iH = 4, iR = 5, iD = 6, idE = 7,
               idH = 8;
--- inst/include/daedalus_helpers.h
+++ inst/include/daedalus_helpers.h
@@ -11,21 +11,33 @@ namespace daedalus {
 /// @brief Helpful functions. May be split up later.
 namespace helpers {
 
+// NOTE: this function handles up to 3 strata (compartments, age, vax status)
+// more general solutions should probably be written in R and pass a vector
+
 /// @brief Get compartments to zero
 /// @param seq_data_compartments A sequence of compartments holding data in the
 /// single-stratum case.
 /// @param n_strata The number of strata; age-groups or other.
+/// @param n_vax The number of vaccination groups, or any strata in the third
+/// dimension.
 /// @return A vector of compartments which
 inline std::vector<size_t> zero_which(
-    const std::vector<size_t> &seq_data_compartments, const int &n_strata) {
+    const std::vector<size_t> &seq_data_compartments, const int &n_strata,
+    const int &n_vax) {
   std::vector<size_t> i_to_zero;
+
   std::vector<int> seq_strata(n_strata);
   std::iota(seq_strata.begin(), seq_strata.end(), 1);
 
+  std::vector<int> seq_vax(n_vax);
+  std::iota(seq_vax.begin(), seq_vax.end(), 1);
+
   for (const auto &i : seq_data_compartments) {
     for (const auto &j : seq_strata) {
-      // cppcheck-suppress useStlAlgorithm
-      i_to_zero.push_back(static_cast<size_t>(i * n_strata - j));
+      for (const auto &k : seq_vax) {
+        // cppcheck-suppress useStlAlgorithm
+        i_to_zero.push_back(static_cast<size_t>(i * n_strata * k - j));
+      }
     }
   }
 
