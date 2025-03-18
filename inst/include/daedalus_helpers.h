9aa1daa89bb42c7a05afd38c8ddfb78cccdd749b
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-18 14:44:40 +0000

    Generalise Cpp fn zero_which() to get_state_idx()

--- inst/include/daedalus_helpers.h
+++ inst/include/daedalus_helpers.h
@@ -24,15 +24,16 @@ namespace helpers {
 // NOTE: this function handles up to 3 strata (compartments, age, vax status)
 // more general solutions should probably be written in R and pass a vector
 
-/// @brief Get compartments to zero
-/// @param seq_data_compartments A sequence of compartments holding data in the
-/// single-stratum case.
+/// @brief Get compartment idx-es. Primarily used to identify compartments to
+/// zero and for events.
+/// @param seq_compartments A sequence of compartments holding variables in the
+/// single-stratum case. Indexing must begin at 1.
 /// @param n_strata The number of strata; age-groups or other.
 /// @param n_vax The number of vaccination groups, or any strata in the third
 /// dimension.
 /// @return A vector of compartments which
-inline std::vector<size_t> zero_which(
-    const std::vector<size_t> &seq_data_compartments, const int &n_strata,
+inline std::vector<size_t> get_state_idx(
+    const std::vector<size_t> &seq_compartments, const int &n_strata,
     const int &n_vax) {
   std::vector<size_t> i_to_zero;
 
@@ -42,7 +43,7 @@ inline std::vector<size_t> zero_which(
   std::vector<int> seq_vax(n_vax);
   std::iota(seq_vax.begin(), seq_vax.end(), 1);
 
-  for (const auto &i : seq_data_compartments) {
+  for (const auto &i : seq_compartments) {
     for (const auto &j : seq_strata) {
       for (const auto &k : seq_vax) {
         // cppcheck-suppress useStlAlgorithm
