b21afb9ad24975c58d148d5b960de256a3e4c5e6
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-18 14:43:15 +0000

    Add flag relative positions

--- inst/include/daedalus_constants.h
+++ inst/include/daedalus_constants.h
@@ -31,6 +31,11 @@ constexpr size_t i_GRPS = 0, i_COMPS = 1, i_VAX_GRPS = 2;
 // for C++ < 20
 inline const std::vector<size_t> i_DATA_COMPARTMENTS = {7, 8};
 inline const std::vector<size_t> seq_DATA_COMPARTMENTS = {8, 9};
+
+// flag positions relative to state size
+constexpr int i_rel_GROWTH_FLAG = 0;
+constexpr int i_rel_RESP_FLAG = 1;
+constexpr int i_rel_VAX_FLAG = 2;
 }  // namespace constants
 
 }  // namespace daedalus
