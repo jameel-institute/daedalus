11cfcf795537f6dc885bb677b034dc7115a1be96
Pratik Gupte <pratikgupte16@gmail.com>
2025-03-19 18:51:56 +0000

    Add events setting and reading from flags

--- inst/include/daedalus_constants.h
+++ inst/include/daedalus_constants.h
@@ -33,6 +33,7 @@ inline const std::vector<size_t> i_DATA_COMPARTMENTS = {7, 8};
 inline const std::vector<size_t> seq_DATA_COMPARTMENTS = {8, 9};
 
 // flag positions relative to state size
+// NOTE: GROWTH_FLAG may be continuous as use case changes
 constexpr int i_rel_GROWTH_FLAG = 0;
 constexpr int i_rel_RESP_FLAG = 1;
 constexpr int i_rel_VAX_FLAG = 2;
