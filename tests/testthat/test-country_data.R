a28980823206a711c681649831e3869bf648b9a1
Pablo Noel Perez Guzman <pperezgu@dide.local>
2025-05-01 13:36:54 +0100

    ported country_data tests to daedalus.data

--- tests/testthat/test-country_data.R
+++
@@ -1,8 +0,0 @@
-# snapshot tests for country data to flag unexpected changes
-# check some countries
-test_countries <- c("China", "United Kingdom", "Canada")
-test_that("daedalus::country_data: snapshot tests", {
-  expect_snapshot(daedalus::country_data[test_countries])
-
-  expect_snapshot(daedalus::country_data[["Canada"]])
-})
