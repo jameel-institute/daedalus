b374b503062e2ede9b50cecde56d581c689ef70b
Pratik Gupte <pratikgupte16@gmail.com>
2025-07-08 16:32:24 +0100

    Update daedalus model funs, vignettes, tests, and news

--- tests/testthat/test-class-response.R
+++ tests/testthat/test-class-response.R
@@ -93,14 +93,6 @@ test_that("daedalus_response: Validator checks", {
     ),
     "(id_state_on)*(must be a vector of integer-ish numbers)"
   )
-  expect_error(
-    new_daedalus_response(
-      "low",
-      "daedalus_vaccination",
-      id_state_on = c(1, NA_integer_, 2)
-    ),
-    "(id_state_on)*(must be a vector of integer-ish numbers)"
-  )
 
   expect_error(
     new_daedalus_response(
@@ -118,14 +110,6 @@ test_that("daedalus_response: Validator checks", {
     ),
     "(id_state_off)*(must be a vector of integer-ish numbers)"
   )
-  expect_error(
-    new_daedalus_response(
-      "low",
-      "daedalus_vaccination",
-      id_state_off = c(1, NA_integer_, 2)
-    ),
-    "(id_state_off)*(must be a vector of integer-ish numbers)"
-  )
 
   expect_error(
     new_daedalus_response(
@@ -135,28 +119,12 @@ test_that("daedalus_response: Validator checks", {
     ),
     "(value_state_on)*(must be a numeric vector)"
   )
-  expect_error(
-    new_daedalus_response(
-      "low",
-      "daedalus_vaccination",
-      value_state_on = c(1.5, NA_real_, 2.0)
-    ),
-    "(value_state_on)*(must be a numeric vector)"
-  )
 
   expect_error(
     new_daedalus_response(
       "low",
       "daedalus_vaccination",
       value_state_off = -1.0
-    ),
-    "(value_state_off)*(must be a numeric vector)"
-  )
-  expect_error(
-    new_daedalus_response(
-      "low",
-      "daedalus_vaccination",
-      value_state_off = c(1.5, NA_real_, 2.0)
     ),
     "(value_state_off)*(must be a numeric vector)"
   )
