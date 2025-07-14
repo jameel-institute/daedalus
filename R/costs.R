1017fc6234664268c0d4deef97ebf9ef6d3bb312
Pratik Gupte <pratikgupte16@gmail.com>
2025-07-14 16:22:53 +0100

    WIP fiscal costs function

--- R/costs.R
+++ R/costs.R
@@ -217,3 +217,205 @@ get_value_school_year <- function(gni) {
   # no checking on GNI for this internal function
   get_value_lost_earnings() * gni * edu_annual_ror
 }
+
+#' Get pandemic fiscal costs from a model run
+#'
+#' @description
+#' A helper function that post-processes a `<daedalus_output>` object to
+#' calculate the costs to a national government (fiscal costs) of responding to
+#' a pandemic. Includes costs of economic support, vaccinations given, and NPIs
+#' administered or implemented.
+#'
+#' @param x A `<daedalus_output>` object.
+#'
+#' @param support_level The proportion of pandemic-related economic losses that
+#' a government compensates, as a proportion.
+#'
+#' @param price_vax The per-dose price of vaccination.
+#'
+#' @param price_npi The per-day and per-person price of implementing any
+#' pandemic response. May include costs such as testing or masks.
+#'
+#' @param uptake_npi The extent to which NPIs are taken up by the population;
+#' essentially a number that modifies (reduces) the total cost of implementing
+#' an NPI.
+#'
+#' @param interest_rate The annual interest rate on government borrowing for
+#' pandemic response.
+#'
+#' @param tax_rate The annual mean tax rate on economic output; used to
+#' calculate government revenue.
+#'
+#' @param spending_rate The annual mean rate of public spending as a percentage
+#' of GDP.
+#'
+#' @return A two-element list giving:
+#'
+#' - A list of `fiscal_costs` with elements giving the daily fiscal costs
+#' \eqn{\text{TCG_t}} and a breakdown of these costs, as well as interest;
+#'
+#' - A time-series `daily_public_debt` of the public debt \eqn{b_t}, which is
+#' the net of baseline public spending, pandemic response costs, and
+#' pandemic-impacted revenue.
+#'
+#' @details
+#' Default argument values are intended to be representative. This
+#' function is intended to be called after [daedalus()] and parameters
+#' required to compute fiscal costs may need to be bundled along with model
+#' outputs.
+#' Note that all rates (interest rate, spending rate, and tax rate) are given as
+#' annual percentages, and are simply divided by 365 to give the daily
+#' rate.
+#'
+#' @export
+#'
+#' @examples
+#' # get fiscal costs for UK with SARS-CoV-2
+#' # and both closures and vaccinations
+#' o <- daedalus("GBR", "sars_cov_2_pre_alpha", "economic_closures", "high")
+#' get_fiscal_costs(o)
+#'
+#' # also works when no closures are applied
+#' o <- daedalus("CAN", "influenza_2009")
+#' get_fiscal_costs(o)
+get_fiscal_costs <- function(
+  x,
+  support_level = 0.33,
+  price_vax = 1.0,
+  price_npi = 1.0,
+  uptake_npi = 1.0,
+  interest_rate = 4.0,
+  tax_rate = 25.0,
+  spending_rate = 45.0
+) {
+  # Needs better error messages
+  checkmate::assert_class(x, "daedalus_output")
+  checkmate::assert_number(
+    support_level,
+    lower = 0,
+    upper = 1
+  )
+  checkmate::assert_number(
+    price_vax,
+    lower = 0,
+    finite = TRUE
+  )
+  checkmate::assert_number(
+    price_npi,
+    lower = 0,
+    finite = TRUE
+  )
+  checkmate::assert_number(
+    uptake_npi,
+    lower = 0,
+    upper = 1
+  )
+  checkmate::assert_number(
+    interest_rate,
+    lower = 0,
+    upper = 100
+  )
+  checkmate::assert_number(
+    tax_rate,
+    lower = 0,
+    upper = 100
+  )
+  checkmate::assert_number(
+    spending_rate,
+    lower = 0,
+    upper = 100
+  )
+
+  gva <- x$country_parameters$gva
+  openness <- x$response_data$openness
+
+  model_data <- get_data(x)
+  worker_absences <- model_data[
+    model_data$compartment %in%
+      c("infect_symp", "infect_asymp", "hospitalised", "dead") &
+      model_data$econ_sector != "sector_00",
+  ]
+  worker_absences <- tapply(
+    worker_absences$value,
+    list(worker_absences$time, worker_absences$econ_sector),
+    sum
+  )
+  workforce <- x$country_parameters$workers
+
+  # calculate labour available after absences and closures
+  # during closures, available labour is the lesser of healthy workers and
+  # mandated capacity
+  avail_labour <- 1.0 - worker_absences %*% diag(1.0 / workforce)
+
+  # calculate closure duration if any
+  closure_duration <- x$response_data$closure_info$closure_duration
+
+  if (is.na(closure_duration)) {
+    npi_support <- 0.0
+  } else {
+    closure_start <- x$response_data$closure_info$closure_time_start
+    closure_end <- x$response_data$closure_info$closure_time_end
+    closure_period <- seq(closure_start, closure_end)
+    
+    # cost of getting NPIs to work: price_npi * number of alive individuals *
+    # some uptake param
+    npi_support <- sum(x$country_parameters$demography) -
+      get_incidence(x, "deaths")$value * uptake_npi
+    npi_support[-closure_period] <- 0.0
+    npi_support <- npi_support / 1e6
+
+    avail_labour[closure_period, ] <- t(apply(
+      avail_labour[closure_period, ],
+      1L,
+      pmin,
+      openness
+    ))
+  }
+
+  # cost of support for GVA loss per sector per day
+  gva_achieved <- rowSums(avail_labour %*% diag(gva))
+  gva_support <- rowSums((1.0 - avail_labour) %*% diag(support_level * gva))
+
+  # cost of vaccination assumed to be instantaneous
+  # NOTE: this is only over the model horizon!
+  vax_support <- get_new_vaccinations(x)$new_vaccinations * price_vax / 1e6
+
+  total_support <- gva_support + vax_support + npi_support
+  names(total_support) <- NULL
+
+  # get total fiscal cost with interest rate
+  interest_rate <- interest_rate / 365.0
+  fiscal_cost <- Reduce(
+    function(x, y) {
+      y + (interest_rate * x)
+    },
+    total_support,
+    accumulate = T
+  )
+
+  # get total public debt, net of spending and revenue
+  daily_spending <- sum(gva) * spending_rate / 365.0 # mean daily spend
+  tax_rate <- tax_rate / 365.0
+
+  public_debt <- numeric(x$total_time + 1L)
+  public_debt[1L] <- daily_spending - tax_rate * gva_achieved[1L]
+
+  for (i in seq(2, x$total_time + 1)) {
+    public_debt[i] <- daily_spending +
+      total_support[i - 1] +
+      (interest_rate * public_debt[i - 1]) -
+      (tax_rate * gva_achieved[i - 1])
+  }
+
+  # return cost timeseries
+  list(
+    fiscal_costs = list(
+      daily_fiscal_cost = fiscal_cost,
+      daily_gva_support = gva_support,
+      daily_vax_support = vax_support,
+      daily_npi_support = npi_support,
+      interest_value = sum(fiscal_cost - total_support)
+    ),
+    daily_public_debt = public_debt
+  )
+}
