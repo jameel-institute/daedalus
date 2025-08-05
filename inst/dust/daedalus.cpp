1df248ec30558f8265dc3216309ac147cfbd9823
Pablo Noel Perez Guzman <pperezgu@dide.local>
2025-08-05 15:50:55 +0100

    assign hospital transitions

--- inst/dust/daedalus.cpp
+++ inst/dust/daedalus.cpp
@@ -519,25 +519,28 @@ class daedalus_ode {
     treats HFR as the age-specific probability of death conditional on being
     hospitalised
      
-    1. Determine individuals who will progress from I_s to H
-        internal.iToH = shared.eta * t_x.chip(iIs, i_COMPS);
+    1. Split internal.isToH = shared.eta * t_x.chip(iIs, i_COMPS); into
+
+        internal.isToHd = isToH * hfr;
+        internal.isToHr = isToH - Hd;
      
-    2. Calculate effective HFR given VE
-        const TensorMat eff_hfr = hfr * (1 - ve_death);
-     
-    3. Movement of isToHd given eff_HFR
-        internal.Hd = itoH * eff_hfr;
-     
-    4. Remainder goes to Hr
-        internal.Hr = iToH - Hd;
-     
-    5. Transitions out ofr Hr and Hd are determined by gamma parameters
+    2. Transitions out ofr Hr and Hd are determined by gamma parameters
         internal.hrToR = shared.gamma_H_recovery * t_x.chip(Hr, i_COMPS);
         internal.hdToD = shared.gamma_H_death * t_x.chip(Hd, i_COMPS);
+     
+    Also TODO:
+     - Declare additional compartments in daedalus_constants.h
+     - initial_state in R/model_helpers.R
+     - flodia model diagram
+     - info_model_description.Rmd vignette
+     - rename omega_modifier to hfr_modifier
+     - check modifier value (* 1.6) data sources
      
     */
     
     internal.isToH = shared.eta * t_x.chip(iIs, i_COMPS);
+    internal.isToHd = isToH * hfr;
+    internal.isToHr = isToH - internal.isToHd;
     internal.hToR = shared.gamma_H * t_x.chip(iH, i_COMPS);
 
     internal.rToS = shared.rho * t_x.chip(iR, i_COMPS);
