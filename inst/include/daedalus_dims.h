12e8999dcde5c9c83c2aea4ef81307b578c4c130
Pratik Gupte <pratikgupte16@gmail.com>
2026-02-27 13:11:32 +0000

    WIP fixing rank 3 tensor compilation warning, use daedalus_types

--- inst/include/daedalus_dims.h
+++ inst/include/daedalus_dims.h
@@ -6,15 +6,20 @@
 #include <RcppEigen.h>
 
 #include "daedalus_constants.h"
+#include "daedalus_types.h"
 
 namespace daedalus {
 namespace dims {
 
-inline const Eigen::array<Eigen::Index, 2> dim_bcast_vax = {
+inline const daedalus::types::bcast_dim_type dim_bcast_vax = {
     1, daedalus::constants::N_VAX_STRATA};
 
-inline const Eigen::array<Eigen::IndexPair<int>, 1> dim_product = {
+inline const daedalus::types::prod_dim_type dim_product = {
     Eigen::IndexPair<int>(1, 0)};
+
+// for Tensor3D.contract(Tensor1D, dims)
+inline const daedalus::types::prod_dim_type vec_tensor_dims = {
+    Eigen::IndexPair<int>(2, 0)};
 
 }  // namespace dims
 }  // namespace daedalus
